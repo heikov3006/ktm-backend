6a8c9ba9f70eedaffa38e2ad585d6d1e98065fda
heikov3006 <h.vichtbauer@students.htl-leonding.ac.at>
2025-02-26 14:08:46 +0100

    build.sh update

--- backend/build.sh
+++ backend/build.sh
@@ -5,6 +5,7 @@ set -e
 rm -rf target
 mvn -B clean package -Dmaven.test.skip=true
 mkdir -p target/deploy
+mkdir -p /opt/application
 cp ./target/*-runner.jar /opt/application/backend.jar
 
 docker buildx build --platform linux/amd64 --tag backend --file ./src/main/docker/Dockerfile .
